22d9b2c59b2ee3330db7e82c94ffa2151bfc0a77
Emirality <vlad.burak1997@list.ru>
2021-07-17 22:31:33 +0300

    Singleton

---
+++ src/SingletonConsumer.java
@@ -0,0 +1,29 @@
+public class SingletonConsumer {
+    public static void main(String[] args) {
+
+        Singleton s1 = Singleton.getInstance("Light");
+        Singleton s2 = Singleton.getInstance("Emy");
+
+        System.out.println(s1.value);
+        System.out.println(s2.value);
+    }
+}
+
+class Singleton{
+    private static Singleton instance;
+    public String value;
+
+    private Singleton(String value){
+        this.value = value;
+    }
+
+    public static Singleton getInstance(String value){
+        if (Singleton.instance != null) {
+            return instance;
+        }
+        return instance = new Singleton(value);
+    }
+}
+
+
+
